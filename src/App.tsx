bc8b345caaccb923024b13672f566e55c6a71056
kristivent <ventk84@gmail.com>
2024-12-26 16:53:56 -0500

    updated Navbar code and App.tsx to reflect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav';
+import CandidateSearch from './pages/CandidateSearch';
+import SavedCandidates from './pages/SavedCandidates';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <>
+    <Router>
       <Nav />
-      <main>
-        <Outlet />
-      </main>
-    </>
+      <Routes>
+        <Route path="/" element={<CandidateSearch />} />
+        <Route path="/saved-candidates" element={<SavedCandidates />} />
+        <Route path="*" element={<ErrorPage />} /> {/* Catch-all route for undefined routes */}
+      </Routes>
+    </Router>
   );
 }
 
