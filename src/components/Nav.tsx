08795942caff186913c8fb2cd979e8588bf311c5
kristivent <ventk84@gmail.com>
2024-12-27 14:07:41 -0500

    added components files and redid pages codes based on errors in console

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,41 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
+  const currentPage = useLocation().pathname;
+
   return (
-    <nav className="navbar">
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">Home</Link>
+    <nav>
+      <h1> 
+        <Link to='/' id='logo'>
+        Candidate Search
+        </Link>
+      </h1>
+      <ul className='nav nav-tabs'>
+        <li className='nav-item'>
+          <h2>
+            <Link
+              to='/'
+              className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+            >
+              HOME
+            </Link>
+          </h2>
+          </li>
+        <li className='nav-item'>
+          <h2>
+            <Link
+              to='/SavedCandidates'
+              className={
+                currentPage === '/SavedCandidates' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Potential Candidates
+            </Link>
+          </h2>
         </li>
-        <li className="nav-item">
-          <Link to="/saved-candidates" className="nav-link">Potential Candidates</Link>
-        </li>
-      </ul>
+        </ul>
     </nav>
   );
-};
+};    
 
 export default Nav;
